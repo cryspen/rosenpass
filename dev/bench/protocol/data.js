243460c1f774472cb78bcadda2596ccc724de0d2
github-action-benchmark <github@users.noreply.github.com>
2025-04-29 17:37:22 +0000

    add rosenpass protocol benchmarks benchmark result for 444e34b98385883a74dbdbdd565cfca1432fb441

--- dev/bench/protocol/data.js
+++ dev/bench/protocol/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745948239888,
+  "lastUpdate": 1745948242081,
   "repoUrl": "https://github.com/cryspen/rosenpass",
   "entries": {
     "rosenpass protocol benchmarks": [
@@ -567,6 +567,576 @@ window.BENCHMARK_DATA = {
           {
             "name": "ici7",
             "value": "7454",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jan@cryspen.com",
+            "name": "Jan Winkelmann (keks)",
+            "username": "keks"
+          },
+          "committer": {
+            "email": "jan@cryspen.com",
+            "name": "Jan Winkelmann (keks)",
+            "username": "keks"
+          },
+          "distinct": true,
+          "id": "444e34b98385883a74dbdbdd565cfca1432fb441",
+          "message": "fix paths",
+          "timestamp": "2025-04-29T19:34:47+02:00",
+          "tree_id": "1523c701383859978315e2a2106c96809606a10c",
+          "url": "https://github.com/cryspen/rosenpass/commit/444e34b98385883a74dbdbdd565cfca1432fb441"
+        },
+        "date": 1745948239741,
+        "bigger_is_better": false,
+        "benches": [
+          {
+            "name": "rhi6",
+            "value": "7193",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr6",
+            "value": "2360554",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr4",
+            "value": "14236",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr6",
+            "value": "60",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "handle_init_conf",
+            "value": "2156912",
+            "unit": "ns/iter",
+            "range": "± 658534",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "handle_init_hello",
+            "value": "92059430",
+            "unit": "ns/iter",
+            "range": "± 1412978",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "handle_initiation",
+            "value": "5595246",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr3",
+            "value": "13966",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr4",
+            "value": "124974",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr7",
+            "value": "1132953",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr1",
+            "value": "1970512",
+            "unit": "ns/iter",
+            "range": "± 846245",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhi5",
+            "value": "85939581",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ici7",
+            "value": "12393",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr2",
+            "value": "89648",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi4",
+            "value": "16100",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhi4",
+            "value": "123141",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr5",
+            "value": "1827866",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr7",
+            "value": "14597",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi5",
+            "value": "1791838",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi7",
+            "value": "1132492",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr4",
+            "value": "18289",
+            "unit": "ns/iter",
+            "range": "± 2259",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr5",
+            "value": "87308341",
+            "unit": "ns/iter",
+            "range": "± 484613",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi3",
+            "value": "86813",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi8",
+            "value": "14136",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr6",
+            "value": "16285",
+            "unit": "ns/iter",
+            "range": "± 876",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ici4",
+            "value": "13926",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr3",
+            "value": "14196",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhi3",
+            "value": "14046",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhi7",
+            "value": "15168",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr7",
+            "value": "14207",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "handle_resp_hello",
+            "value": "86148523",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihr8",
+            "value": "14077",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "rhr1",
+            "value": "811",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi2",
+            "value": "270",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "handle_resp_conf",
+            "value": "1126821",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ici3",
+            "value": "14237",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr1",
+            "value": "2089420",
+            "unit": "ns/iter",
+            "range": "± 725835",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi1",
+            "value": "1226398",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "ihi6",
+            "value": "1144925",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V02"
+          },
+          {
+            "name": "icr7",
+            "value": "13845",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi1",
+            "value": "2938237",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhi5",
+            "value": "87664153",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi3",
+            "value": "85620",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi6",
+            "value": "2829703",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "icr3",
+            "value": "16050",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr1",
+            "value": "2811308",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "icr6",
+            "value": "51",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "handle_resp_conf",
+            "value": "2822901",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ici7",
+            "value": "13986",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr3",
+            "value": "15869",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr5",
+            "value": "3413067",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr4",
+            "value": "20428",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr6",
+            "value": "17092",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr7",
+            "value": "2827099",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ici4",
+            "value": "15700",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "icr1",
+            "value": "3027282",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi7",
+            "value": "2814064",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi2",
+            "value": "291",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi8",
+            "value": "15780",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi5",
+            "value": "3481123",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "handle_init_hello",
+            "value": "102653754",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr8",
+            "value": "15719",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr1",
+            "value": "221",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "handle_initiation",
+            "value": "12330478",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihr5",
+            "value": "87748832",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr4",
+            "value": "133730",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ici3",
+            "value": "15940",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "handle_init_conf",
+            "value": "3139343",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "icr2",
+            "value": "64150",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "handle_resp_hello",
+            "value": "87882031",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "ihi4",
+            "value": "21050",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhi7",
+            "value": "17352",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "icr4",
+            "value": "15790",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhi4",
+            "value": "128942",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr6",
+            "value": "5633357",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhr7",
+            "value": "15799",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhi6",
+            "value": "8767",
+            "unit": "ns/iter",
+            "range": "± 1",
+            "category": "proto_run_V03"
+          },
+          {
+            "name": "rhi3",
+            "value": "15959",
             "unit": "ns/iter",
             "range": "± 1",
             "category": "proto_run_V03"
